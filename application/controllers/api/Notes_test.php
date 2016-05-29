cf24171efcd5d106457bf2d14c43fd961dee4cf7
ajaragoneses <aj.aragoneses@gmail.com>
2016-05-29 13:47:40 +0200

    Modelo notas y primeros test

--- application/controllers/api/Notes_test.php
+++ application/controllers/api/Notes_test.php
@@ -6,30 +6,31 @@ class Notes_test extends CI_Controller {
         $this->unit->set_test_items(array('test_name', 'expected_result', 'result'));
         $str = '{rows}{item} : {result}<br />{/rows}<br/>';
         $this->unit->set_template($str); 
+
+        $this->load->model('notes');
 	}
 
-	private function sumaCorrectaTest(){
-		$test = 1 + 1;
-        $expected_result = 2;
-        $test_name = 'Adds one plus one';
-        $this->unit->run($test, $expected_result, $test_name);
+	private function getterTest(){
+		$test = $this->notes->getAttr('table');
+		$expected_result = "notes";
+		$test_name = "getter table name test";
+		$this->unit->run($test, $expected_result, $test_name);
 	}
 
-	private function sumaIncorrectaTest(){
-		$test = 1 + 1;
-        $expected_result = 3;
-        $test_name = 'Adds one plus one';
-        $this->unit->run($test, $expected_result, $test_name);
+	private function setterTest(){
+		$this->notes->setAttr('note', "BLRBLRBLRPOIPOIPOI");
+		$test = $this->notes->getAttr('note');
+		$expected_result = "BLRBLRBLRPOIPOIPOI";
+		$test_name = "setter table name test";
+		$this->unit->run($test, $expected_result, $test_name);
 	}
-
 
 	public function test()
 	{
 		$this->init_test();
-		$this->sumaCorrectaTest();
-		$this->sumaIncorrectaTest();
+		$this->getterTest();
+		$this->setterTest();
         echo $this->unit->report();
 	}
 
 }
-?>
