4784fcf37b0e10de9122873ad35a94a8d6cdc554
ajaragoneses <aj.aragoneses@gmail.com>
2016-05-30 21:39:13 +0200

    añadida ruta api obtencion todas notas

---
+++ application/models/Notes_model.php
@@ -0,0 +1,16 @@
+<?php
+include_once('GenericModel.php');
+
+class Notes_model extends GenericModel {
+
+    protected $table = 'notes';
+    protected $note = "";
+    protected $id_user = 0;
+
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
+}
